8c78def783d7d048f5b8dda972e72ebf12dcdecb
leedongwook-kr <3eks@naver.com>
2021-03-21 14:56:55 +0900

    Start project

---
+++ ns.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var counter = 1;
+setInterval(function () {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 5) {
+        counter = 1;
+    }
+}, 5000);
